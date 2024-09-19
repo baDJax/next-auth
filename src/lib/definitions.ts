361ce7f7495168678002e4b93691769a1b421730
baDJax <mukulsharma262002@gmail.com>
2024-09-19 12:57:48 +0530

    Fixed the typo error

--- src/lib/definitions.ts
+++ src/lib/definitions.ts
@@ -12,7 +12,7 @@ export const SignupFormSchema = z.object({
   password: z
     .string()
     .min(8, { message: "Be at least 8 characters long" })
-    .regex(/[a-zA-Z]/, { message: "Contain al least one letter" })
+    .regex(/[a-zA-Z]/, { message: "Contain at least one letter" })
     .regex(/[0-9]/, { message: "Contain at least one number" })
     .regex(/[^a-zA-Z0-9]/, {
       message: "Contain at least one special character",
