586b312e54e9d6299b2ecf42ab83f1f95562741b
baDJax <mukulsharma262002@gmail.com>
2024-09-19 12:23:51 +0530

    Added styles and input data in signup form

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -1,43 +1,37 @@
 "use client";
 const SignupForm = () => {
+  const styles = {
+    inputStyle:
+      "block w-full px-3 py-1 rounded border-2 focus:outline-none focus:border-slate-950 border-slate-700",
+    formStyle: "max-w-[450px] w-full mx-auto px-6 p-10 border border-slate-500/10 rounded shadow-2xl"
+  };
+
+  const inputData = [
+    { label: "Name", id: "name", type: "text" },
+    { label: "Email", id: "email", type: "email" },
+    { label: "Password", id: "password", type: "password" },
+  ];
+
   return (
     <form
-      className="max-w-[450px] w-full mx-auto px-3 py-5 border border-slate-500/10 rounded shadow-2xl"
+      className={styles?.formStyle}
       action={() => console.log("form submitted")}
     >
       <div className="py-2 text-center">
         <h2 className="text-3xl font-semibold">Signup</h2>
       </div>
-      <div className="py-2">
-        <label htmlFor="name">Name</label>
-        <input
-          id="name"
-          className="block w-full px-3 py-1 rounded border-2 focus:outline-none focus:border-slate-950 border-slate-700"
-          type="text"
-          name="name"
-          placeholder="Enter your name"
-        />
-      </div>
-      <div className="py-2">
-        <label htmlFor="email">Email</label>
-        <input
-          id="email"
-          className="block w-full px-3 py-1 rounded border-2 focus:outline-none focus:border-slate-950 border-slate-700"
-          type="email"
-          name="email"
-          placeholder="Enter your email"
-        />
-      </div>
-      <div className="py-2">
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          className="block w-full px-3 py-1 rounded border-2 focus:outline-none focus:border-slate-950 border-slate-700"
-          type="password"
-          name="password"
-          placeholder="Enter your password"
-        />
-      </div>
+      {inputData?.map(({ label, id, type }) => (
+        <div key={id} className="py-2">
+          <label htmlFor={id}>{label}</label>
+          <input
+            id={id}
+            className={styles?.inputStyle}
+            type={type}
+            name={id}
+            placeholder={`Enter your ${label}`}
+          />
+        </div>
+      ))}
       <div className="py-2">
         <button
           className="px-3 py-2 rounded bg-slate-800 text-white w-full"
