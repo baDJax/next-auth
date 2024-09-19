5a40580685ae1f99107ce7493aa511d9508dd81a
baDJax <mukulsharma262002@gmail.com>
2024-09-19 12:14:44 +0530

    Updated tailwind config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      container:{
+        padding: '1rem',
+        center: true,
+      }
     },
   },
   plugins: [],
